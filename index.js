7dd8cd3e8ebb978e2de1fca5f3a0e6d485583512
Michael Craddock <michael.craddock@lv.co.uk>
2022-05-31 12:48:06 +0100

    doing some array adding logic

--- index.js
+++ index.js
@@ -21,3 +21,13 @@ console.log(multiple([1,2,3,4,5,6,7,8,9]));
 
 //console.log(multipleNums);
 console.log(addNumbers(a,b));
+
+// doing things with new multiple
+const array1 = [1,2,3,4,5,6,7,8,9];
+const array2 = [1,1,1,1,1,1,1,1,1,1];
+
+const addArrays = addNumbers(multiple(array1), multiple(array2));
+
+console.log(addArrays);
+
+
