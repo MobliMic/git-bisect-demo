f7680e98ac9b617af2bdcd29a8820c8c11e527a9
Michael Craddock <michael.craddock@lv.co.uk>
2022-05-31 12:41:37 +0100

    multiple addition

--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const addNumbers = (num1, num2) => {
 	return num1 + num2;
 }
 
+const addMultiple = (array) => {
+	return array.reduce();
+}
+
+const multipleNums = addMultiple([1,2,3,4,5,6,7,8,9]);
+
+console.log(multipleNums);
 console.log(addNumbers(a,b));
